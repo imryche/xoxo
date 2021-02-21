66adc60855664d22d47bf2964e5e58164d6c135f
oneor0 <oneor00@gmail.com>
2021-02-21 19:01:25 +0300

    implement OAuth2 authentication

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
+from datetime import datetime, timedelta
 from typing import Optional
 
 import databases
 import sqlalchemy as sa
-from sqlalchemy.dialects.postgresql import ARRAY
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi import status as http_status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from pydantic import BaseModel
+from sqlalchemy.dialects.postgresql import ARRAY
 
 from xoxo.game import (
     BoardStatus,
@@ -14,12 +20,23 @@ from xoxo.game import (
     print_board,
 )
 
+DATABASE_URL = os.environ["DATABASE_URL"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-DATABASE_URL = "postgres://localhost/xoxo"
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sa.MetaData()
+
+users = sa.Table(
+    "users",
+    metadata,
+    sa.Column("id", sa.Integer, primary_key=True),
+    sa.Column("username", sa.String, nullable=False),
+    sa.Column("password", sa.String, nullable=False),
+)
 
 moves = sa.Table(
     "moves",
@@ -49,7 +66,91 @@ class PlayerMove(BaseModel):
         return all(v is not None for v in (self.row, self.col))
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+
+
+class UserInDB(User):
+    password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+async def get_user(username):
+    query = users.select().where(users.c.username == username)
+    user = await database.fetch_one(query)
+    if user:
+        return UserInDB(**user)
+
+
+async def authenticate_user(username: str, password: str):
+    user = await get_user(username)
+    if not user:
+        return False
+
+    if not verify_password(password, user.password):
+        return False
+
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=http_status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials.",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+
+    user = await get_user(token_data.username)
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 
 @app.on_event("startup")
@@ -106,7 +207,30 @@ async def play(player_move: PlayerMove):
     return {"status": status, "move": ai_move, "board": board}
 
 
-@app.get("/moves/")
-async def read_moves():
+@app.get("/moves")
+async def read_moves(token: str = Depends(oauth2_scheme)):
     query = moves.select()
     return await database.fetch_all(query)
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=http_status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
