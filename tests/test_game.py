48a33e4a5e778700b9a5249fb6146cae4f56b856
oneor0 <oneor00@gmail.com>
2021-02-21 11:02:06 +0300

    ai logic improvements

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,11 +1,11 @@
 import pytest
-from game import (
-    find_best_move,
-    get_possible_board,
-    get_possible_moves,
+from xoxo.game import (
     board_score,
-    cells_score,
-    move
+    find_best_move,
+    get_possible_moves,
+    make_move,
+    undo_move,
+    cells_score
 )
 
 
@@ -15,7 +15,7 @@ def test_maximizing_makes_move():
         [None, False, True],
         [False, None, False]
     ]
-    move(board, 0, 1, True)
+    make_move(board, (0, 1), True)
     assert board == [
         [True, True, True],
         [None, False, True],
@@ -29,7 +29,7 @@ def test_non_maximizing_makes_move():
         [None, False, True],
         [False, None, False]
     ]
-    move(board, 2, 1, False)
+    make_move(board, (2, 1), False)
     assert board == [
         [True, None, True],
         [None, False, True],
@@ -44,20 +44,35 @@ def test_makes_forbidden_move():
         [False, None, False]
     ]
     with pytest.raises(ValueError):
-        move(board, 0, 0, False)
+        make_move(board, (0, 0), False)
 
 
-def test_evaluates_maximizing_score():
-    row = [True, True, True]
-    assert cells_score(row, 3) == 7
+def test_undoes_move():
+    board = [
+        [True, None, True],
+        [None, False, True],
+        [False, None, False]
+    ]
+    undo_move(board, (1, 1))
+    assert board[1][1] is None
 
 
-def test_evaluates_non_maximizing_score():
-    row = [False, False, False]
-    assert cells_score(row, 3) == -7
+def test_cells_have_positive_score():
+    cells = [True, True, True]
+    assert cells_score(cells) == 10
 
 
-def test_horizontal_win():
+def test_cells_have_negative_score():
+    cells = [False, False, False]
+    assert cells_score(cells) == -10
+
+
+def test_cells_have_no_score():
+    cells = [False, None, True]
+    assert cells_score(cells) is None
+
+
+def test_horizontal_true_win():
     board = [
         [False, None, None],
         [True, True, True],
@@ -66,7 +81,25 @@ def test_horizontal_win():
     assert board_score(board) == 10
 
 
-def test_vertical_win():
+def test_horizontal_false_win():
+    board = [
+        [True, None, None],
+        [False, False, False],
+        [True, None, True]
+    ]
+    assert board_score(board) == -10
+
+
+def test_vertical_true_win():
+    board = [
+        [False, None, True],
+        [None, False, True],
+        [None, False, True]
+    ]
+    assert board_score(board) == 10
+
+
+def test_vertical_false_win():
     board = [
         [True, None, False],
         [None, True, False],
@@ -93,13 +126,22 @@ def test_diagonal_two_win():
     assert board_score(board) == -10
 
 
+def test_tie():
+    board = [
+        [True, True, False],
+        [False, False, True],
+        [True, False, True]
+    ]
+    assert board_score(board) == 0
+
+
 def test_nobody_won():
     board = [
         [True, None, False],
         [None, True, None],
         [None, True, False]
     ]
-    assert board_score(board) == 0
+    assert board_score(board) is None
 
 
 def test_possible_moves():
@@ -122,46 +164,31 @@ def test_no_possible_moves_left():
     assert get_possible_moves(board) == []
 
 
-def test_possible_board_created():
-    board = [
-        [True, None, None],
-        [False, False, True],
-        [False, True, False]
-    ]
-    possible_board = get_possible_board(board, (0, 1), is_maximizing=True)
-    assert possible_board == [
-        [True, True, None],
-        [False, False, True],
-        [False, True, False]
-    ]
-    assert board is not possible_board
-
-
 def test_find_obvious_moves():
     board = [
-        [True, False, True],
-        [False, False, True],
+        [False, False, None],
+        [False, True, None],
+        [True, None, None]
+    ]
+    assert find_best_move(board) == (0, 2)
+
+    board = [
+        [False, True, None],
+        [None, True, None],
         [None, None, None]
     ]
-    assert find_best_move(board) == (2, 2)
+    assert find_best_move(board) == (2, 1)
 
     board = [
-        [True, False, False],
-        [None, False, None],
-        [None, True, True]
-    ]
-    assert find_best_move(board) == (2, 0)
-
-    board = [
-        [True, False, True],
-        [True, False, False],
-        [None, None, False]
-    ]
-    assert find_best_move(board) == (2, 0)
-
-    board = [
-        [True, False, True],
-        [False, False, None],
-        [None, None, True]
+        [False, True, False],
+        [True, True, None],
+        [True, False, None]
     ]
     assert find_best_move(board) == (1, 2)
+
+    board = [
+        [False, False, None],
+        [True, True, False],
+        [True, None, None]
+    ]
+    assert find_best_move(board) == (0, 2)
---
+++ xoxo/game.py
@@ -0,0 +1,101 @@
+import math
+
+
+def print_board(board):
+    for row in board:
+        for cell in row:
+            if cell is None:
+                symbol = "_"
+            else:
+                symbol = "x" if cell else "o"
+            print(symbol, end="")
+        print("")
+    print()
+
+
+def make_move(board, move, player):
+    if board[move[0]][move[1]] is None:
+        board[move[0]][move[1]] = player
+    else:
+        raise ValueError(f"Cell ({move[0]},{move[1]}) is occupied.")
+
+
+def undo_move(board, move):
+    board[move[0]][move[1]] = None
+
+
+def cells_score(cells):
+    if all(cell == cells[0] and cell is not None for cell in cells):
+        return 10 if cells[0] else -10
+
+
+def board_score(board, depth=0):
+    for row in board:
+        if score := cells_score(row):
+            return score
+
+    size = len(board)
+    for col_idx in range(size):
+        col = [board[row][col_idx] for row in range(size)]
+        if score := cells_score(col):
+            return score
+
+    diagonals = [
+        [board[i][i] for i in range(size)],
+        [board[i][size - 1 - i] for i in range(size)],
+    ]
+
+    for diagonal in diagonals:
+        if score := cells_score(diagonal):
+            return score
+
+    if not get_possible_moves(board):
+        return 0
+
+
+def get_possible_moves(board):
+    moves = []
+    size = len(board)
+    for row in range(size):
+        for col in range(size):
+            if board[row][col] is None:
+                moves.append((row, col))
+    return moves
+
+
+def minimax(board, depth, is_maximizing):
+    score = board_score(board, depth)
+    if score is not None:
+        return score
+
+    if is_maximizing:
+        best_score = -math.inf
+        for move in get_possible_moves(board):
+            make_move(board, move, True)
+            score = minimax(board, depth + 1, True)
+            undo_move(board, move)
+            best_val = max([score, best_score])
+        return best_val
+    else:
+        best_score = math.inf
+        for move in get_possible_moves(board):
+            make_move(board, move, False)
+            score = minimax(board, depth + 1, False)
+            undo_move(board, move)
+            best_val = min([score, best_score])
+        return best_val
+
+
+def find_best_move(board):
+    best_move = None
+    best_score = -math.inf
+
+    for move in get_possible_moves(board):
+        make_move(board, move, True)
+        score = minimax(board, 0, False)
+        undo_move(board, move)
+        if score > best_score:
+            best_score = score
+            best_move = move
+
+    return best_move
